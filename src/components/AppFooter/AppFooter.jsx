50334d7491118e98a1a570882137d9d6d0c5de98
Daran Dachte <darandachte@gmail.com>
2023-03-12 16:24:52 +0100

    footer created

---
+++ src/components/AppFooter/AppFooter.jsx
@@ -0,0 +1,66 @@
+import { Group, ActionIcon, Anchor } from "@mantine/core";
+import { NavLink } from "react-router-dom";
+import {
+  IconBrandLinkedin,
+  IconBrandGithub,
+  IconMail,
+} from "@tabler/icons-react";
+
+import { useStyles } from "./AppFooter.style";
+
+const links = [
+  { link: "/", label: "Catalog" },
+  { link: "/about", label: "About" },
+  { link: "/privacy", label: "Privacy" },
+  { link: "/contact", label: "Contact" },
+];
+
+const AppFooter = () => {
+  const { classes } = useStyles();
+  const items = links.map((link) => (
+    <NavLink key={link.label} to={link.link}>
+      {link.label}
+    </NavLink>
+  ));
+
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.inner}>
+        <p>LOGO</p>
+
+        <Group className={classes.links}>{items}</Group>
+
+        <Group spacing="xs" position="right" noWrap>
+          <ActionIcon
+            component="a"
+            href="https://www.linkedin.com/in/daran-dachte-031820249/"
+            size="lg"
+            variant="default"
+            radius="xl"
+          >
+            <IconBrandLinkedin size="1.05rem" stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon
+            component="a"
+            href="https://github.com/DaranDachte"
+            size="lg"
+            variant="default"
+            radius="xl"
+          >
+            <IconBrandGithub size="1.05rem" stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon
+            component="a"
+            href="mailto:darandachte@gmail.com"
+            size="lg"
+            variant="default"
+            radius="xl"
+          >
+            <IconMail size="1.05rem" stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </div>
+    </footer>
+  );
+};
+export default AppFooter;
