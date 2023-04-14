946154ba0cfa9032f23262581f6bc9fffd9bd691
Daran Dachte <darandachte@gmail.com>
2023-04-14 14:04:49 +0200

    contact form was created

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,63 @@
+import { useStyles } from "./Contact.style";
+import {
+  Paper,
+  Text,
+  TextInput,
+  Textarea,
+  Button,
+  Group,
+  SimpleGrid,
+} from "@mantine/core";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const { classes } = useStyles();
+
+  return (
+    <Paper shadow="md" radius="lg">
+      <div className={classes.wrapper}>
+        <div className={classes.contacts}>
+          <Text fz="lg" fw={700} className={classes.title} c="#fff">
+            Contact information
+          </Text>
+        </div>
+
+        <form
+          className={classes.form}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Text fz="lg" fw={700} className={classes.title}>
+            Get in touch
+          </Text>
+
+          <div className={classes.fields}>
+            <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+              <TextInput label="Your name" placeholder="Your name" />
+              <TextInput
+                label="Your email"
+                placeholder="hello@mantine.dev"
+                required
+              />
+            </SimpleGrid>
+
+            <TextInput mt="md" label="Subject" placeholder="Subject" required />
+
+            <Textarea
+              mt="md"
+              label="Your message"
+              placeholder="Please include all relevant information"
+              minRows={3}
+            />
+
+            <Group position="right" mt="md">
+              <Button type="submit" className={classes.control}>
+                Send message
+              </Button>
+            </Group>
+          </div>
+        </form>
+      </div>
+    </Paper>
+  );
 };
 
 export default Contact;
